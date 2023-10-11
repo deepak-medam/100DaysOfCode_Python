cb13846538a391e45a718a252440612943898b39
deepak-medam <109440747+deepak-medam@users.noreply.github.com>
2023-10-11 10:51:27 +0100

    Completed Day 04 files

---
+++ Day_04/exe_4.1_heads_or_tails.py
@@ -0,0 +1,17 @@
+# You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+
+# Important, the first letter should be capitalised and spelt exactly like in the example e.g. "Heads", not "heads".
+
+# There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number,
+# either 0 or 1. Then use that number to print out "Heads" or "Tails".
+
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+h_or_t = random.randint(0,1)
+
+if h_or_t == 1:
+    print("Heads")
+else:
+    print("Tails")
---
+++ Day_04/lists.py
@@ -0,0 +1,8 @@
+states_of_america = ['Delaware', 'Pensnsylvania', 'South Carolina', 'Texas', 'Utah', 'Vermont', 'Virginia',
+                     'Washington', 'West Virginia', 'Wisconsin' , 'Wyoming', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',]
+
+print(states_of_america[-0])
+
+test_tree = ['apple', ['grape', ['tomato']]]
+
+print(test_tree[1][1][0])
