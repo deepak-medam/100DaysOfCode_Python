3e6688733acb6c86311442a4208a8c7b569280f7
deepak-medam <109440747+deepak-medam@users.noreply.github.com>
2023-10-12 07:02:24 +0100

    Completed Day 06 files

---
+++ Day_06/karel_square.py
@@ -0,0 +1,22 @@
+# Draw a square with karel.
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def square():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_around()
+
+square()
